ac2051618aed5d7326760fb8bb1cf7c0b42ad4ab
Tomas Karasek <tom.to.the.k@gmail.com>
2015-02-10 18:59:45 +0200

    removed HTTP

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ func main() {
 
 	flag.StringVar(&config.nameserver, "nameserver", config.nameserver, "DNS server for unmatched requests")
 	flag.StringVar(&config.dnsAddr, "dns", config.dnsAddr, "Listen DNS requests on this address")
-	flag.StringVar(&config.httpAddr, "http", config.httpAddr, "Listen HTTP requests on this address")
 	domain := flag.String("domain", config.domain.String(), "Domain that is appended to all requests")
 	environment := flag.String("environment", "", "Optional context before domain suffix")
 	flag.StringVar(&config.dockerHost, "docker", config.dockerHost, "Path to the docker socket")
@@ -52,13 +51,6 @@ func main() {
 	if err := docker.Start(); err != nil {
 		log.Fatal(err)
 	}
-
-	httpServer := NewHTTPServer(config, dnsServer)
-	go func() {
-		if err := httpServer.Start(); err != nil {
-			log.Fatal(err)
-		}
-	}()
 
 	if err := dnsServer.Start(); err != nil {
 		log.Fatal(err)
